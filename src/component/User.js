e3496854c3fa40067c093301fb6ecbcdcb863bb7
wang <xodhkd334@naver.com>
2022-12-07 13:56:12 +0900

    add onremove

--- src/component/User.js
+++ src/component/User.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const User = ({ user }) => {
+const User = ({ user, onRemove }) => {
   return (
     <div>
       <span>{user.username}</span>
       <b>({user.email})</b>
+      <button onClick={() => onRemove(user.id)}>Delete</button>
     </div>
   );
 };
--- src/component/UserList.js
+++ src/component/UserList.js
@@ -1,11 +1,11 @@
 import React from "react";
 import User from "./User";
 
-const UserList = ({ users }) => {
+const UserList = ({ users, onRemove }) => {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} />
+        <User user={user} key={user.id} onRemove={onRemove} />
       ))}
     </div>
   );
